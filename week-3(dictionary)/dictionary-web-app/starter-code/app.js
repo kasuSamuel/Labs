f00fedfc70c0e11f91efe008265cc7680d10f8ee
Kasu Samuel <kasusamuel17@gmail.com>
2024-11-20 19:11:48 +0000

    commit

--- week-3(dictionary)/dictionary-web-app/starter-code/app.js
+++ week-3(dictionary)/dictionary-web-app/starter-code/app.js
@@ -3,20 +3,38 @@ const fontSelector = document.getElementById('font-selector');
 const searchBar = document.querySelector('.search-bar');
 const imgElement = document.querySelector('.toggle-wrapper img');
 const searchInput = document.querySelector('.search-input');
+const options = document.querySelectorAll(' option');
 
 modeToggle.addEventListener('change', () => {
   document.documentElement.setAttribute('data-theme', modeToggle.checked ? 'dark' : 'light');
   searchBar.style.backgroundColor = modeToggle.checked? '#757575' : '#f4f4f4';
   imgElement.src = modeToggle.checked ? './assets/images/icon-dark-moon.svg' : './assets/images/icon-moon.svg';
-    // document.body.style.color = modeToggle.checked ? '#fff' : '#000';
+  fontSelector.classList.toggle('white-text');
+  options.forEach(option => {
+    option.style.backgroundColor = modeToggle.checked ? '#121212' : '#ffffff';
+    option.style.color = modeToggle.checked ? 'white' : 'black'; 
+  }); 
+
+  
 });
   searchInput.addEventListener('click', function() {
-    searchBar.style.borderColor = '#A445ED';  // Just changes the border color
-    searchBar.style.borderStyle = 'solid';   // Ensures the border is solid
+    searchBar.style.borderColor = '#A445ED';  
+    searchBar.style.borderStyle = 'solid';   
     searchBar.style.borderWidth = '1px';
   });
   
 
-fontSelector.addEventListener('change', (e) => {
-  document.body.style.fontFamily = e.target.value === 'serif' ? 'Georgia, serif' : 'Arial, sans-serif';
+  document.addEventListener('click', function(event) {
+    // Check if the click was not inside the searchBar or searchInput
+    if (!searchBar.contains(event.target) && !searchInput.contains(event.target)) {
+        searchBar.style.borderColor = 'none';  
+        searchBar.style.borderStyle = 'none';  
+        searchBar.style.borderWidth = '0';     
+    }
 });
+
+fontSelector.addEventListener('change', (e) => {
+    document.body.style.fontFamily = e.target.value === 'serif'? 'Georgia, serif'
+: (e.target.value === 'mono' ? 'Mono, monospace' : 'Arial, sans-serif');
+  });
+  
